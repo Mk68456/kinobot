efd633b3adef9b1678d64c61dd495e7b1cbfc752
Verrrtex <88099302+Mk68456@users.noreply.github.com>
2026-09-12 22:50:04 +0300

    Add files via upload

--- handlers/users/start_command.py
+++ handlers/users/start_command.py
@@ -1,31 +1,13 @@
-from database.channels import get_all_channels_links
 from aiogram import types
 from loader import dp, bot
 from aiogram.dispatcher.filters import CommandStart
 from database.people import add_user
-from keyboards.inline.sub_keyboard import sub_markup
-from keyboards.users.keyboard import find_movie_markup
+from services.welcome import send_welcome
 from .check_user_sub import check_user_sub
 
 
-@dp.message_handler(CommandStart())
-async def start_command_handler(message: types.Message):
+@dp.message_handler(CommandStart(), state="*")
+async def start_command_handler(message: types.Message, state):
+    await state.finish()
     add_user(message.from_user.id, username=message.from_user.username)
-    check = await check_user_sub(message)
-    if check != False:
-        await bot.send_message(
-            message.chat.id,
-            "Нажмите на кнопку ниже, чтобы найти фильм 👇🏻",
-            reply_markup=find_movie_markup(),
-        )
-    else:
-        await bot.send_message(
-            message.chat.id,
-            "ЧТО МОЖЕТ ДЕЛАТЬ ЭТОТ БОТ?\n\n"
-            "Привет, это 🍿KinoTime - Бот для скачивания и просмотра фильмов на твоём телефоне\n\n"
-            "Чтобы скачать или посмотреть фильм из Youtube или ТИК ТОК, нажмите на все ссылки снизу👇🏻 и ПОДПИШИТЕСЬ НА ВСЕ КАНАЛЫ\n\n"
-            "(подписка на каналы занимает 5 секунд)\n\n"
-            "После подписки нажмите на кнопку:\n"
-            "✔️ Я ПОДПИСАЛСЯ ✔️ 👇🏻 и там уже узнаете название фильмов♥️",
-            reply_markup=sub_markup(get_all_channels_links()),
-        )
+    await send_welcome(bot, message.chat.id, subscribed=bool(await check_user_sub(message)))
