1e855ae68a9053a72b250a97ee559e7ebb014abe
Verrrtex <88099302+Mk68456@users.noreply.github.com>
2026-09-12 23:02:44 +0300

    Add files via upload

--- handlers/users/start_command.py
+++ handlers/users/start_command.py
@@ -1,13 +1,3 @@
-from aiogram import types
-from loader import dp, bot
-from aiogram.dispatcher.filters import CommandStart
-from database.people import add_user
-from services.welcome import send_welcome
-from .check_user_sub import check_user_sub
+"""Compatibility import; registration is performed first by handlers.start."""
 
-
-@dp.message_handler(CommandStart(), state="*")
-async def start_command_handler(message: types.Message, state):
-    await state.finish()
-    add_user(message.from_user.id, username=message.from_user.username)
-    await send_welcome(bot, message.chat.id, subscribed=bool(await check_user_sub(message)))
+from handlers.start import start_command_handler  # noqa: F401
